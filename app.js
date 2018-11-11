d41903b2ceddfa880eb91ff5ecfc92eda0845960
dipaksingbhukwal <35825317+dipaksingbhukwal@users.noreply.github.com>
2018-11-10 20:28:35 -0800

    Handled Auth

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
-
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send('It works');
-})
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
 
 // Use Routes
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-  console.log(`Server started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://dipaksingbhukwal:Sadguru@24@ds159263.mlab.com:59263/storybook-csuf-dev',
-  googleClientID: '990663010172-3sal765k7saeql49nvb9qvg4bqph558s.apps.googleusercontent.com',
-  googleClientSecret: 'd2bF94GupMw1VlV53zQnhy5f'
+  mongoURI:'mongodb://localuser:Welcome123@ds159263.mlab.com:59263/storybook-csuf-dev',
+  googleClientID:'990663010172-3sal765k7saeql49nvb9qvg4bqph558s.apps.googleusercontent.com',
+  googleClientSecret:'d2bF94GupMw1VlV53zQnhy5f'
 }
